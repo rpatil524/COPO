57a5a1cdbee201d52abbfe15627cff39ee1c57b5
shaw2thefloor <shaw2thefloor@gmail.com>
2016-09-13 11:37:34 +0000

    set .env variables

--- web/src/web/settings/data.py
+++ web/src/web/settings/data.py
@@ -6,11 +6,11 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASS'],
-        'HOST': os.environ['DB_SERVICE'],
-        'PORT': os.environ['DB_PORT']
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_SERVICE'],
+        'PORT': os.environ['POSTGRES_PORT']
     }
 }
 
