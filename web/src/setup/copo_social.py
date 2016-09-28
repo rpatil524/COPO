4adcb4e7d8f4d3035d4a33d2d2dc09b8474da391
Felix Shaw <felix.shaw@tgac.ac.uk>
2016-09-28 11:08:09 +0100

    fix socialapp duplicate ids

--- web/src/setup/copo_social.py
+++ web/src/setup/copo_social.py
@@ -44,8 +44,8 @@ def main():
         print("Creating 'socialaccount_socialapp' records...\n")
         cursor.execute("INSERT INTO socialaccount_socialapp (id, provider, name, client_id, secret, key) VALUES (%s, %s, %s, %s, %s, %s)", (1, "google", "Google", "197718904608-mubhgir39dr8e159ef4hb3l5i8me71b6.apps.googleusercontent.com", os.environ['GOOGLE_SECRET'], " "))
         cursor.execute("INSERT INTO socialaccount_socialapp (id, provider, name, client_id, secret, key) VALUES (%s, %s, %s, %s, %s, %s)", (2, "orcid", "Orcid", "APP-EGMH46B26C2OCJ9F", os.environ['ORCID_SECRET'], " "))
-        cursor.execute("INSERT INTO socialaccount_socialapp (id, provider, name, client_id, secret, key) VALUES (%s, %s, %s, %s, %s, %s)", (1, "facebook", "Facebook", "497282503814650", os.environ['FACEBOOK_SECRET'], " "))
-        cursor.execute("INSERT INTO socialaccount_socialapp (id, provider, name, client_id, secret, key) VALUES (%s, %s, %s, %s, %s, %s)", (1, "twitter", "Twitter", "qrwJCJG9aBngGnBKrnvwgGNYc", os.environ['TWITTER_SECRET'], " "))
+        cursor.execute("INSERT INTO socialaccount_socialapp (id, provider, name, client_id, secret, key) VALUES (%s, %s, %s, %s, %s, %s)", (3, "facebook", "Facebook", "497282503814650", os.environ['FACEBOOK_SECRET'], " "))
+        cursor.execute("INSERT INTO socialaccount_socialapp (id, provider, name, client_id, secret, key) VALUES (%s, %s, %s, %s, %s, %s)", (4, "twitter", "Twitter", "qrwJCJG9aBngGnBKrnvwgGNYc", os.environ['TWITTER_SECRET'], " "))
 
         print("Creating 'socialaccount_socialapp_sites' record...\n")
         cursor.execute("INSERT INTO socialaccount_socialapp_sites (id, socialapp_id, site_id) VALUES (%s, %s, %s)", (1,1,3))
