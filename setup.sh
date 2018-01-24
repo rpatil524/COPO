5129dd14ba65fa3ef4150df6943fa4f190a57158
Felix Shaw <fshaw@Felixs-MacBook-Pro.local>
2018-01-24 18:30:15 +0000

    adding installs for further COPO dependencies

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ divider() {
   s=$(printf "%-30s" "-")
   echo "${s// /-}"
 }
-
+div=divider
 # embedded python function to get system info
 function check_os {
 python - <<END
@@ -22,12 +22,9 @@ END
 }
 
 
-
-div=divider
-
-printf 'This script will install a development ready instance of COPO onto
-your machine along with all its dependencies. If you are on a Mac, please
-ensure you have Homebrew installed before running this script.\n'
+# START
+printf 'Welcome to the installation program for COPO. This will install a development ready instance of COPO onto
+your machine along with all its dependencies. This script is only garanteed to work on OSX and Ubuntu installations'
 
 $div
 
@@ -63,3 +60,34 @@ $pac_man update
 
 # first determine the current OS
 os=check
+
+# now check dependencies are installed
+
+if [ $opsys == "Darwin" ]
+then
+  # install python3
+  $pac_man install python3
+  # install MongoDB
+  $pac_man install mongodb
+  echo making data/db/
+  sudo mkdir -p /data/db
+  brew services start mongodb
+  # install redis
+  $pac_man install redis
+  $pac_man services start redis
+elif [ $opsys == "Ubuntu" ]
+then
+  $pac_man --allow-unauthenticated -y install python3
+  # import public key used by apt-get
+  $pac_man install apt-transport-https
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+  # create list file for MongoDB
+  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+  $pac_man update
+  # install and start mongo
+  $pac_man install -y mongodb-org
+  mongod --fork --config /etc/mongod.conf
+  # install and start redis
+  $pac_man install redis-server
+  service redis-server start
+fi
