d36b51a701c27b96c0c89a204f310cac114a1697
Felix Shaw <fshaw@Felixs-MBP.lan>
2018-01-25 17:35:45 +0000

    psql setup working in Darwin and Ubuntu

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+postgres_user='postgres_user'
+postgres_db='copo'
+postgres_pw='Apple123'
+
+python -c "exit"
+if [ "$?" -eq 127 ]
+then
+  echo "Python not found on this system. Please install any version of python and try again"
+  exit
+fi
+
 divider() {
   s=$(printf "%-30s" "-")
   echo "${s// /-}"
@@ -30,7 +41,8 @@ $div
 
 opsys=$(check_os)
 printf 'Determining OS: %s\n' "$opsys"
-
+me=$(whoami)
+printf 'Installing under user accoutn: %s\n' "$me"
 printf 'Checking Package Manager\n'
 
 if [ $opsys == "Darwin" ]
@@ -66,9 +78,9 @@ os=check
 if [ $opsys == "Darwin" ]
 then
   # install python3
-  $pac_man install python3
+  $pac_man install -y python3
   # install MongoDB
-  $pac_man install mongodb
+  $pac_man install -y mongodb
   echo making data/db/
   sudo mkdir -p /data/db
   brew services start mongodb
@@ -78,11 +90,19 @@ then
   # install postgres
   $pac_man install postgresql
   $pac_man services start postgresql
+  # create psql user and db
+  sudo -u $me createuser $postgres_user
+  sudo -u $me createdb $postgres_db
+  psql postgres $me -c "alter user $postgres_user with encrypted password '$postgres_pw';"
+  psql postgres $me -c "grant all privileges on database $postgres_db to $postgres_user ;"
+
+
+
 elif [ $opsys == "Ubuntu" ]
 then
-  $pac_man --allow-unauthenticated -y install python3
+  $pac_man --allow-unauthenticated install -y python3
   # import public key used by apt-get
-  $pac_man install apt-transport-https
+  $pac_man install -y apt-transport-https
   apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
   # create list file for MongoDB
   echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list
@@ -91,6 +111,21 @@ then
   $pac_man install -y mongodb-org
   mongod --fork --config /etc/mongod.conf
   # install and start redis
-  $pac_man install redis-server
+  $pac_man install -y redis-server
   service redis-server start
+  $pac_man install -y postgresql postgresql-contrib
+  service postgresql start
+  # install sudo for postgres setup
+  $pac_man install -y sudo
+  # TODO change thse later for user input strings
+  # create psql user and db
+  $pac_man install sed
+  # need to change postgres to password authentication
+  sed -i 's/peer/md5/g' /etc/postgresql/$(ls /etc/postgresql)/main/pg_hba.conf
+  sudo -u postgres createuser $postgres_user
+  sudo -u postgres createdb $postgres_db
+  sudo -u postgres psql -c "alter user $postgres_user with encrypted password '$postgres_pw';"
+  sudo -u postgres psql -c "grant all privileges on database $postgres_db to $postgres_user;"
+
+
 fi
