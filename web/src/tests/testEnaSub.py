b7cd46574060807f4ff944d9d0226641e5429418
shaw2thefloor <felix.shaw@tgac.ac.uk>
2018-02-06 17:36:40 +0000

    creating test data and json fixture for ena test....need to dummy user login

--- web/src/tests/testEnaSub.py
+++ web/src/tests/testEnaSub.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
-from .test_utilities.get_test_db_connection import get_client
+from .test_utilities.get_test_db_connection import get_client, load_fixtures
 from django.conf import settings
 from dal.copo_da import Profile
+import os
 
 class Ena_Tests(TestCase):
 
     db_name = settings.MONGO_TEST_DB_NAME
+    d = os.getcwd()
+    fix = load_fixtures('web/src/tests/test_data/ena_test_data.json')
 
     def setUp(self):
         self.client = get_client()
@@ -14,7 +17,7 @@ class Ena_Tests(TestCase):
         settings.MONGO_CLIENT = self.client
 
     def test_can(self):
-        self.db.test.insert_one({'str': "str123"})
+        Profile().save_record(self.fix['profile'])
 
 
     def tearDown(self):
--- web/src/tests/test_utilities/get_test_db_connection.py
+++ web/src/tests/test_utilities/get_test_db_connection.py
@@ -1,15 +1,21 @@
 # Created by fshaw at 05/02/2018
 from django.conf import settings
 from pymongo import MongoClient
+import json
 
 
 def get_client():
-    """ GetDB - simple function to wrap getting a database
-    connection from the connection pool.
-    """
+    #GetDB - simple function to wrap getting a database
+    #connection from the connection pool.
+
     client = MongoClient(host=settings.MONGO_HOST,
                          port=settings.MONGO_PORT,
                          maxIdleTimeMS=1000 * 60)
     return client
 
 
+def load_fixtures(file_location):
+    # load test data from json file
+    with open(file_location) as json_data:
+        d = json.load(json_data)
+        return d
