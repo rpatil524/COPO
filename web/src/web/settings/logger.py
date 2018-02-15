6c2374dbcbbca4d2454e7e8234dcd2d9760a1eba
shaw2thefloor <felix.shaw@tgac.ac.uk>
2018-02-15 15:33:05 +0000

    minor change to logger prefs

--- web/src/web/settings/logger.py
+++ web/src/web/settings/logger.py
@@ -4,7 +4,7 @@ from web.apps.web_copo.lookup.copo_enums import Loglvl, Logtype
 from exceptions_and_logging.logger import Logger
 import exceptions_and_logging.logs
 
-log_path = os.path.dirname(exceptions_and_logging.logs.__file__)
+log_path = os.path.join(os.getcwd(), 'web', 'src', 'exceptions_and_logging', 'logs')
 
 # set the log level to the lowest level of logging you want to see
 # i.e. for only errors, set to Loglvl.ERROR, for errors and warnings, Loglvl.WARNING, and for all, Loglvl.INFO
