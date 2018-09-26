a605752c04941b3758a82f478252a679cd1b761a
shaw2thefloor <felix.shaw@tgac.ac.uk>
2018-09-26 17:14:46 +0100

    submission of single items to dspace working!!!

--- web/src/submission/dspaceSubmission.py
+++ web/src/submission/dspaceSubmission.py
@@ -1,6 +1,6 @@
 # Created by fshaw at 13/09/2018
 from django.http import HttpResponse
-from dal.copo_da import Submission
+from dal.copo_da import Submission, DataFile
 import requests
 import json
 from web.apps.web_copo.schemas.utils import data_utils
@@ -29,7 +29,7 @@ class DspaceSubmit(object):
 
         # if dataset id in submission meta, we are adding to existing dataset, otherwise
         #  we are creating a new dataset
-        if 'dspace_item_name' in s['meta'] :
+        if 'dspace_item_name' in s['meta']:
             # submit to existing
             self._add_to_dspace(s)
         else:
@@ -37,16 +37,59 @@ class DspaceSubmit(object):
             self._create_and_add_to_dspace(s)
         return True
 
-
     def _add_to_dspace(self, sub):
-        print(sub)
-        return True
 
+        dspace_url = sub['destination_repo']['url']
+        email = sub['destination_repo']['username']
+        password = sub['destination_repo']['password']
+        login_url = dspace_url + "/rest/login"
+        resp = requests.post(login_url, {"email": email, "password": password})
+        login_details = resp.cookies["JSESSIONID"]
+        item_id = sub['meta']['identifier']
+
+        for s in sub['bundle']:
+            f = DataFile().get_record(ObjectId(s))
+            name = f['name']
+            location = f['file_location']
+            description = f['description']['attributes']['subject_description']['description']
+            # files = {name: open(location, 'rb')}
+
+            bitstream_url = dspace_url + "/rest/items/" + item_id + "/bitstreams?name=" + name + "&description=" + description
+            headers = {"Content-Type": "application/json", "accept": "application/json"}
+            policy = [{"action": "DEFAULT_*", "epersonId": -1, "groupId": 0, "resourceId": 47166,
+                       "resourceType": "bitstream", "rpDescription": None, "rpName": None, "rpType": "TYPE_INHERITED",
+                       "startDate": None, "endDate": None}]
+            bitstream = {"name": name,
+                         "type": "bitstream",
+                         "bundleName": "ORIGINAL",
+                         "policies": policy,
+                         }
+
+            resp = requests.post(bitstream_url, data=bitstream, headers=headers, cookies={"JSESSIONID": login_details})
+            if resp.status_code == 200:
+                c = resp.content.decode('utf-8')
+                data = json.loads(c)
+                fi = open(location, 'rb')
+                if "uuid" in data:
+                    data_url = dspace_url + "/rest/bitstreams/" + data["uuid"] + "/data"
+                else:
+                    data_url = dspace_url + "/rest/bitstreams/" + data["id"] + "/data"
+                data_resp = requests.put(data_url, data=fi, headers=headers, cookies={"JSESSIONID": login_details})
+                if data_resp.status_code == 200:
+                    self._update_submission(sub, data_resp)
+            else:
+                return (str(resp.status_code) + " ," + resp.reason + " ," + resp.content)
+        logout_url = dspace_url + '/rest/logout'
+        requests.post(logout_url, cookies={"JSESSIONID": login_details})
+
+        return True
 
     def _create_and_add_to_dspace(self, sub):
         print(sub)
         return True
 
+    def _update_submission(self, sub, data_resp):
+        print(data_resp)
 
     def get_dspace_communites(self):
         url = self.host['url']
