87989e59288d3b9ddfe6e6c008ca0c4196da016b
Felix Shaw <felix.shaw@tgac.ac.uk>
2019-05-23 17:50:55 +0100

    multiple sheets now displaying in bootstrap tabs with handsontable

--- static/copo/js/copo_annotate_spreadsheet.js
+++ static/copo/js/copo_annotate_spreadsheet.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    // attach array to document which will be used to hold spreadsheet data
+    $(document).data('ss_data', new Array())
     refresh_display()
 })
 
@@ -8,8 +10,39 @@ function refresh_display() {
         url: "/copo/refresh_annotation_display/",
         data: {"file_id": file_id},
         type: "GET",
+
     }).done(function (data) {
-        $("#ss_data").html(data)
+        data = JSON.parse(data)
+        $("#ss_data").empty()
+        //$("#ss_sheets").empty()
+        $(data.data).each(function (idx, d) {
+            if (idx == 0) {
+                var active = "in active"
+            } else {
+                var active = ""
+            }
+            var li = '<li class="' + active + '"><a data-toggle="tab" href="#' + data.names[idx] + '">' + data.names[idx] + '</a></li>'
+            $('#ss_sheets').append(li)
+
+            var h = $('<div class="tab-pane fade ' + active + '" id="' + data.names[idx] + '"></div>')
+
+            $("#ss_data").append(h)
+
+            var cells = document.getElementById(data.names[idx])
+            //d = JSON.parse(d)
+
+            new Handsontable(cells, {
+                data: d,
+                rowHeaders: true,
+                colHeaders: true,
+                filters: true,
+                dropdownMenu: true,
+                licenseKey: 'non-commercial-and-evaluation'
+            });
+
+
+        })
+
     }).error(function (data) {
         console.error(data)
     })
--- web/apps/web_copo/utils/annotation_handlers.py
+++ web/apps/web_copo/utils/annotation_handlers.py
@@ -3,6 +3,10 @@ import pandas
 from django.http import HttpResponse
 import json
 
+# how many rows of each sheet should be shown to the user
+truncate_after = 5
+
+
 def refresh_display(request):
     file_id = request.GET["file_id"]
     file = DataFile().get_record(file_id)
@@ -11,5 +15,10 @@ def refresh_display(request):
     x1 = pandas.ExcelFile(path)
     sheet_names = x1.sheet_names
     for name in sheet_names:
-        data.append(x1.parse(name).to_csv())
-    return HttpResponse(json.dumps(data))
+        d = pandas.read_excel(path, name)
+        out = list()
+        out.append(d.columns.tolist())
+        out.extend(d.values.tolist())
+
+        data.append(out)
+    return HttpResponse(json.dumps({"data": data, "names": sheet_names}))
