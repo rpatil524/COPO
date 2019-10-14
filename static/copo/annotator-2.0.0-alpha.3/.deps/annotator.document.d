5a2555bd67071cfc67efa073e32247643abc15dc
shaw2thefloor <shaw2thefloor@gmail.com>
2019-10-14 15:03:33 +0530

    adding annotator pkg files

---
+++ static/copo/annotator-2.0.0-alpha.3/.deps/annotator.document.d
@@ -0,0 +1,1 @@
+pkg/annotator.document.js: /home/fshaw/Dropbox/PycharmProjects/COPO/static/copo/annotator-2.0.0-alpha.3/src/ext/document.js
---
+++ static/copo/annotator-2.0.0-alpha.3/.deps/annotator.unsupported.d
@@ -0,0 +1,1 @@
+pkg/annotator.unsupported.js: /home/fshaw/Dropbox/PycharmProjects/COPO/static/copo/annotator-2.0.0-alpha.3/src/ext/unsupported.js
