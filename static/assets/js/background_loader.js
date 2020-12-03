7c284bad6fe116025d475c7d3f3823d2e9a86dae
shaw2thefloor <shaw2thefloor@gmail.com>
2020-12-03 16:23:23 +0000

    more minor changes

--- static/assets/js/background_loader.js
+++ static/assets/js/background_loader.js
@@ -5,7 +5,12 @@ $(document).ready(function () {
 
     $('body').css("background-image", "url(" + images[index] + ")")
 
+    try {
+        var color = getRandomInt(content_classes.length)
+        $("#main_banner").addClass(content_classes[color])
+    } catch (err) {
 
+    }
     $('.ui.dropdown')
         .dropdown();
 
