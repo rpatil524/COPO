2e34a052dd5c2ee19a6ee727788e6015c64684b0
shaw2thefloor <shaw2thefloor@gmail.com>
2021-06-08 11:20:44 +0100

    samples chart working

--- static/copo/js/graphing.js
+++ static/copo/js/graphing.js
@@ -1,10 +1,86 @@
 async function draw_line_graph() {
 
-    const data = await d3.json("/api/stats/sample_stats_csv")
+    const dataset = await d3.json("/api/stats/sample_stats_csv")
     // accessors
 
-    const xAccessor = d => d.date
-    const yAccessor = d => d.samples
+    const yAccessor = d => d.num
+    const dateParser = d3.timeParse("%Y-%m-%d")
+    const xAccessor = d => dateParser(d.date)
+
+    let dimensions = {
+        width: window.innerWidth * 0.5,
+        height: 400,
+        margin: {
+            top: 15,
+            right: 15,
+            bottom: 40,
+            left: 60,
+        },
+    }
+    dimensions.boundedWidth = dimensions.width
+        - dimensions.margin.left
+        - dimensions.margin.right
+    dimensions.boundedHeight = dimensions.height
+        - dimensions.margin.top
+        - dimensions.margin.bottom
+
+    //Draw canvas
+    const wrapper = d3.select("#graph")
+        .append("svg")
+        .attr("width", dimensions.width)
+        .attr("height", dimensions.height)
+
+    const bounds = wrapper.append("g")
+        .style("transform", `translate(${
+            dimensions.margin.left
+        }px, ${
+            dimensions.margin.top
+        }px)`)
+
+    // scales
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(dataset, yAccessor))
+        .range([dimensions.boundedHeight, 0])
+    const xScale = d3.scaleTime()
+        .domain(d3.extent(dataset, xAccessor))
+        .range([0, dimensions.boundedWidth])
+
+    //draw
+    const lineGenerator = d3.line()
+        .x(d => xScale(xAccessor(d)))
+        .y(d => yScale(yAccessor(d)))
+
+    const line = bounds.append("path")
+        .attr("d", lineGenerator(dataset))
+        .attr("fill", "none")
+        .attr("stroke", "cornflowerblue")
+        .attr("stroke-width", 3)
+
+    // Draw peripherals
+
+    const yAxisGenerator = d3.axisLeft()
+        .scale(yScale)
+
+    const yAxis = bounds.append("g")
+        .call(yAxisGenerator)
+
+    const xAxisGenerator = d3.axisBottom()
+        .scale(xScale)
+
+    const xAxis = bounds.append("g")
+        .call(xAxisGenerator)
+        .style("transform", `translateY(${
+            dimensions.boundedHeight
+        }px)`)
+
+    const yAxisLabel = yAxis.append("text")
+        .attr("x", -dimensions.boundedHeight / 2)
+        .attr("y", -dimensions.margin.left + 10)
+        .attr("fill", "black")
+        .attr("font-size", "1.4em")
+        .text("Number of Samples")
+        .style("transform", "rotate(-90deg)")
+        .style("text-anchor", "middle")
 }
 
 draw_line_graph()
